0c1053eac7c4d5023e7ff3efef537381aee50906
mohzaey <moh.balogun@gmail.com>
2022-08-16 06:40:40 +0100

    abi

--- helloworrld.c
+++ helloworrld.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
-#include<conio.h>
+
 
 int main()
 {
     printf("Hello world is a starter");
     return 0;
-    print("my name is jack");
+    printf("my name is jack");
     return 0;
     printf("ameri");
     return 0;
